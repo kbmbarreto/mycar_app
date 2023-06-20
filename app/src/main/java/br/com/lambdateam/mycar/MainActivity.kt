c3d9035371a0f395e88b0cd5b0211cc4e91b0fbd
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2023-06-20 02:30:49 -0300

    Adicionadas as primeiras duas telas

--- app/src/main/java/br/com/lambdateam/mycar/MainActivity.kt
+++ app/src/main/java/br/com/lambdateam/mycar/MainActivity.kt
@@ -1,24 +1,93 @@
 package br.com.lambdateam.mycar
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loginButton = findViewById(R.id.buttonLogin)
+        val menuButton = findViewById<ImageButton>(R.id.menu_button)
+        val historyCard = findViewById<CardView>(R.id.history_maintenances)
+        val servicesCard = findViewById<CardView>(R.id.services)
+        val trafficTicketCard = findViewById<CardView>(R.id.history_traffic_ticket)
+        val vehiclesCard = findViewById<CardView>(R.id.vehicles)
 
-        loginButton.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
+        menuButton.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view)
+            popupMenu.inflate(R.menu.menu_main)
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_oficinas -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_veiculos -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_multas -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_lista_compras -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_servicos -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_fabricantes -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_componentes -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_historico_manutencoes -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_lgpd -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.menu_sair -> {
+                        Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+        }
+
+        historyCard.setOnClickListener {
+            Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+        }
+
+        servicesCard.setOnClickListener {
+            Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+        }
+
+        trafficTicketCard.setOnClickListener {
+            Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
+        }
+
+        vehiclesCard.setOnClickListener {
+            Toast.makeText(this, "Em implementação", Toast.LENGTH_SHORT).show()
         }
     }
 }
