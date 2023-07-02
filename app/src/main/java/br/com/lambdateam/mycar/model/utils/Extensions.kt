7df5b94fa76dd971df7d6b37d8f6443ef7394156
Victor Gleidson Sales Batista <victor.batista@MMB027525CPS.local>
2023-07-02 19:11:05 -0300

    AdClose Button

--- app/src/main/java/br/com/lambdateam/mycar/model/utils/Extensions.kt
+++ app/src/main/java/br/com/lambdateam/mycar/model/utils/Extensions.kt
@@ -1,7 +1,11 @@
 package br.com.lambdateam.mycar.model.utils
 
+import android.content.Context
 import android.os.CountDownTimer
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import java.text.NumberFormat
 import java.text.SimpleDateFormat
--- app/src/main/java/br/com/lambdateam/mycar/views/maintenance/MaintenanceHistoryFragment.kt
+++ app/src/main/java/br/com/lambdateam/mycar/views/maintenance/MaintenanceHistoryFragment.kt
@@ -1,13 +1,15 @@
 package br.com.lambdateam.mycar.views.maintenance
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.ViewFlipper
-import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +23,7 @@ class MaintenanceHistoryFragment : Fragment() {
     private lateinit var rvMaintenance: RecyclerView
     private lateinit var vfMaintenance: ViewFlipper
     private lateinit var tvCount: TextView
+    private lateinit var ivCleanSearch: ImageView
     private lateinit var etSearch: EditText
     private val maintenanceAdapter = MaintenanceHistoryAdapter()
     private val viewModel by inject<MaintenanceViewModel>()
@@ -45,14 +48,34 @@ class MaintenanceHistoryFragment : Fragment() {
     private fun setupSearch() {
         etSearch.afterTextChangedDelayed {
             maintenanceAdapter.search(it)
-            tvCount.text = resources.getQuantityString(R.plurals.result_plurals, maintenanceAdapter.itemCount,  maintenanceAdapter.itemCount)
+            tvCount.text = resources.getQuantityString(
+                R.plurals.result_plurals,
+                maintenanceAdapter.itemCount,
+                maintenanceAdapter.itemCount
+            )
+            ivCleanSearch.visibility = if (it.isEmpty()) View.GONE else View.VISIBLE
         }
+        ivCleanSearch.setOnClickListener {
+            etSearch.apply {
+                setText("")
+                clearFocus()
+            }
+            it.visibility = View.GONE
+            closeKeyboard()
+        }
+    }
+
+    private fun closeKeyboard() {
+        val imm: InputMethodManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 
     private fun setupListeners() {
         viewModel.maintenances.observe(viewLifecycleOwner) { list ->
             list?.let {
-                tvCount.text = resources.getQuantityString(R.plurals.result_plurals, it.size,  it.size)
+                tvCount.text =
+                    resources.getQuantityString(R.plurals.result_plurals, it.size, it.size)
                 maintenanceAdapter.setItems(it)
             }
         }
@@ -92,6 +115,7 @@ class MaintenanceHistoryFragment : Fragment() {
             vfMaintenance = it.findViewById(R.id.vf_maintenance_history)
             etSearch = it.findViewById(R.id.et_maintenance_history_search)
             tvCount = it.findViewById(R.id.tv_maintenance_history_count)
+            ivCleanSearch = it.findViewById(R.id.iv_maintenance_history_clean_search)
         }
     }
 }
